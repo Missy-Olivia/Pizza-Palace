2d2685c848b6402a262e4d24e2c1cdf37dceca3d
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-10-13 00:06:24 +0200

    added jquery to append order list after clicking checkout button

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,23 @@ $("#del").click(function () {
     var address = prompt("Enter your address:");
     alert("Your pizza will be delivered to " + address + ".");
 });
+$("#checkout").click(function(event){
+
+    var pieSize = new Array();$("input:checkbox[name='size']:checked").each(function(){
+        pieSize.push(this.value)
+    });
+    var crustType =new Array(); $("input:checkbox[name='crust']:checked").each(function(){
+        crustType.push(this.value)
+    });
+    var topkind =new Array(); $("input:checkbox[name='toppings']:checked").each(function(){
+        topkind.push(this.value)
+    });
+    var pizzaNumber = $("#pizzanumber").val();
+    
+        event.preventDefault();
+        $(".summary").show();
+        $("#summary").append("Pizza Size:" + pieSize + "<br>")
+        $("#summary").append("Type of Crust:" + crustType + "<br>")
+        $("#summary").append("Toppings:" + topkind + "<br>")
+        $("#summary").append("Pizza number:" + pizzaNumber + "<br>")
+    });
